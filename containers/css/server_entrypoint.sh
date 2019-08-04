8d4be949beac1418e5e6c1e630d44803c74587a7
Jesse Young <jesse@monkeybased.com>
2019-08-03 20:16:52 -0500

    revert ip changes

--- containers/css/server_entrypoint.sh
+++ containers/css/server_entrypoint.sh
@@ -9,7 +9,7 @@ $STEAM_GAME_DIR/srcds_run \
     -steam_dir $STEAMCMD_DIR/ \
     -steamcmd_script /steam/game_update.txt \
     -consolelog $STEAM_GAME_DIR/console.log \
-    +ip $IP_ADDRESS \
+    +ip 0.0.0.0 \
     +sv_pure 1 \
     +map de_dust \
     +maxplayers 24 \
