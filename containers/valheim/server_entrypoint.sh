72af674e84f64b82b44811b519e915d693bedeb6
Jesse Young <jesseyoungcomputer@gmail.com>
2021-02-16 12:05:00 -0600

    Set server public

--- containers/valheim/server_entrypoint.sh
+++ containers/valheim/server_entrypoint.sh
@@ -9,4 +9,4 @@ echo "Starting server PRESS CTRL-C to exit"
 # Tip: Make a local copy of this script to avoid it being overwritten by steam.
 # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
 # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
-$STEAM_GAME_DIR/valheim_server.x86_64 -name "The Bar Room" -port 2456 -world "Dedicated" -password "bringbeer" -savedir $VALHEIM_SAVE_DIR
+$STEAM_GAME_DIR/valheim_server.x86_64 -name "The Bar Room" -port 2456 -world "Dedicated" -password "bringbeer" -savedir $VALHEIM_SAVE_DIR -public 1
