b5d4a556681e021a339273bad0805f426cd04164
Jesse Young <jesse@monkeybased.com>
2024-01-30 18:31:35 -0600

    Add palworld to networking init

--- setup_networking.sh
+++ setup_networking.sh
@@ -5,3 +5,4 @@ iptables -t nat -A POSTROUTING -s 10.1.0.0/16 ! -o bridge-tf2 -j SNAT --to-sourc
 iptables -t nat -A POSTROUTING -s 10.3.0.0/16 ! -o bridge-pz -j SNAT --to-source 69.162.82.213
 iptables -t nat -A POSTROUTING -s 10.2.0.0/16 ! -o bridge-css -j SNAT --to-source 69.162.82.211
 iptables -t nat -A POSTROUTING -s 10.4.0.0/16 ! -o bridge-l4d2 -j SNAT --to-source 69.162.82.212
+iptables -t nat -A POSTROUTING -s 10.5.0.0/16 ! -o bridge-palworld -j SNAT --to-source 69.162.82.213
